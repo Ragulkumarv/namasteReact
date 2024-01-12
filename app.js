93b4f421de585a16dd3c663ef4ae9b6dd6823089
Ragul <ragulkumarv333@gmail.com>
2024-01-12 19:55:20 +0530

    removed cdn & used imports, updated gitignore, removed main for build

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const header = React.createElement("h1", { id: "head" }, "hello");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const header = React.createElement("h1", { id: "head" }, "hello Welcome Ragul");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
