736f4326343f3f6b9eec3a57de1424f9e432443e
Ragul <ragulkumarv333@gmail.com>
2024-01-19 12:23:10 +0530

    invoke component inside component

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const header = React.createElement("h1", { id: "head" }, "hello Welcome Ragul");
+const Title = <h1 className="title">Hello this is React . Welcome !!</h1>;
+const JSXHeader = () => (
+  <h2 className="title">Hello this is second Title . Welcome !!</h2>
+);
+
+const FuncComp = () => {
+  return (
+    <div className="container">
+      {Title}
+      <JSXHeader />
+      <h1 className="heading">component composition</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+
+root.render(<FuncComp />);
